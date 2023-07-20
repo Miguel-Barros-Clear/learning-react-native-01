8a70d0e735610000f25c80a58235e2cd148b11d4
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-20 19:04:34 -0300

    feat: created currency conversor app

--- App.tsx
+++ App.tsx
@@ -1,52 +1,23 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Modal } from 'react-native';
-import Entrar from './src/Entrar';
+import { StyleSheet, View, Text } from 'react-native';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modalVisible: false,
-    };
+import Conversor from './src/Conversor';
 
-    this.entrar = this.entrar.bind(this);
-    this.sair = this.sair.bind(this);
-  }
-  entrar() {
-    this.setState({ modalVisible: true });
-  }
-  sair(visible) {
-    this.setState({ modalVisible: visible });
-  }
-
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Entrar" onPress={this.entrar} />
-
-        <Modal
-          transparent={true}
-          animationType="slide"
-          visible={this.state.modalVisible}>
-          <View
-            style={{
-              margin: 15,
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Entrar fechar={() => this.sair(false)} />
-          </View>
-        </Modal>
+        <Conversor moedaA="USD" moedaB="BRL" />
+        <Conversor moedaA="EUR" moedaB="BRL" />
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#DDD',
   },
 });
