509eec6addf030a1b0bd085b29af194c33cbcfd1
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-22 09:12:45 -0300

    feat: transform app to search cep

--- App.tsx
+++ App.tsx
@@ -1,47 +1,88 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
-  Animated,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
 } from 'react-native';
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      LarAnimada: new Animated.Value(50),
-    };
 
-    Animated.timing(this.state.LarAnimada, {
-      toValue: 100,
-      duration: 5000,
-    }).start();
-  }
+export default function App() {
+  const [cep, setCep] = useState('');
 
-  render() {
-    let porcentagemAnimate = this.state.LarAnimada.interpolate({
-      inputRange: [0, 100],
-      outputRange: ['0%', '100%'],
-    });
-    return (
-      <View style={styles.container}>
-        <Animated.View
-          style={{
-            backgroundColor: '#4169E1',
-            width: porcentagemAnimate,
-            height: 25,
-          }}
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.text}>Digite o cep desejado</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 11730000"
+          value={cep}
+          onChangeText={texto => setCep(texto)}
+          keyboardType="numeric"
         />
       </View>
-    );
-  }
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={[styles.botao, { backgroundColor: '#1d75cd' }]}>
+          <Text style={styles.botaoText}>Buscar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.botao, { backgroundColor: '#cd3e1d' }]}>
+          <Text style={styles.botaoText}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.resultado}>
+        <Text style={styles.itemText}>CEP: 1173000</Text>
+        <Text style={styles.itemText}>Logradouro: 11730000</Text>
+        <Text style={styles.itemText}>Bairro: Itaóca</Text>
+        <Text style={styles.itemText}>Cidade: São Paulo</Text>
+        <Text style={styles.itemText}>Estado: SP</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  text: {
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    width: '90%',
+    padding: 10,
+    fontSize: 18,
+  },
+  areaBtn: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'space-around',
+  },
+  botao: {
     justifyContent: 'center',
-    alignItems: 'flex-start',
+    alignItems: 'center',
+    padding: 15,
+    borderRadius: 5,
+  },
+  botaoText: {
+    fontSize: 22,
+    color: '#FFF',
+  },
+  resultado: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 22,
   },
 });
