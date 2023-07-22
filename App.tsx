9d0e29505c350ea3c4dba40906ddcf8ef4c215b0
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-22 09:49:46 -0300

    feat: working with icons

--- App.tsx
+++ App.tsx
@@ -1,120 +1,40 @@
-import React, { useRef, useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  SafeAreaView,
-  Keyboard,
-} from 'react-native';
-import api from './src/services/api';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 export default function App() {
-  const [cep, setCep] = useState('');
-  const inputRef = useRef(null);
-  const [cepUser, setCepUser] = useState();
-
-  async function buscar() {
-    if (cep === '') {
-      alert('Digite um cep válido');
-      setCep('');
-      return;
-    }
-
-    try {
-      const response = await api.get(`/${cep}/json`);
-      console.log(response.data);
-      setCepUser(response.data);
-      Keyboard.dismiss();
-    } catch (error) {
-      console.log('ERROR', +error);
-    }
-  }
-
-  function limpar() {
-    setCep('');
-    inputRef.current.focus();
-    setCepUser(null);
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={{ alignItems: 'center' }}>
-        <Text style={styles.text}>Digite o cep desejado</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Ex: 11730000"
-          value={cep}
-          onChangeText={texto => setCep(texto)}
-          keyboardType="numeric"
-        />
-      </View>
-      <View style={styles.areaBtn}>
-        <TouchableOpacity
-          style={[styles.botao, { backgroundColor: '#1d75cd' }]}
-          onPress={buscar}>
-          <Text style={styles.botaoText}>Buscar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.botao, { backgroundColor: '#cd3e1d' }]}
-          onPress={limpar}>
-          <Text style={styles.botaoText}>Limpar</Text>
-        </TouchableOpacity>
-      </View>
-      {cepUser && (
-        <View style={styles.resultado}>
-          <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
-          <Text style={styles.itemText}>Logradouro: {cepUser.logradouro}</Text>
-          <Text style={styles.itemText}>Bairro: {cepUser.bairro}</Text>
-          <Text style={styles.itemText}>Cidade: {cepUser.localidade}</Text>
-          <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
-        </View>
-      )}
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Sujeito Programador</Text>
+      <FontAwesome name="home" size={35} color="#11118c" />
+      <FontAwesome name="user" size={25} color="#54a300" />
+      <Feather name="gift" size={65} color="#7665ff" />
+
+      <TouchableOpacity style={styles.btnYoutube}>
+        <FontAwesome name="youtube" size={35} color="#FFF" />
+        <Text style={styles.btnText}>Acessar canal</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  text: {
-    marginTop: 25,
-    marginBottom: 15,
-    fontSize: 25,
-    fontWeight: 'bold',
-  },
-  input: {
-    backgroundColor: '#FFF',
-    borderWidth: 1,
-    borderColor: '#DDD',
-    borderRadius: 5,
-    width: '90%',
-    padding: 10,
-    fontSize: 18,
-  },
-  areaBtn: {
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  btnYoutube: {
     flexDirection: 'row',
-    marginTop: 15,
-    justifyContent: 'space-around',
-  },
-  botao: {
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 15,
-    borderRadius: 5,
+    padding: 5,
+    backgroundColor: '#F00',
   },
-  botaoText: {
-    fontSize: 22,
+  btnText: {
+    paddingLeft: 10,
     color: '#FFF',
-  },
-  resultado: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  itemText: {
-    fontSize: 22,
   },
 });
