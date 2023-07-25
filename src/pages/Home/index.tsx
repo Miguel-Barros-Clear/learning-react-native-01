de6b9f62103450876542da01c8de7885f5d2b602
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-25 19:13:23 -0300

    feat: using drawer navigator

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,15 +3,14 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 export default function Home() {
   const navigation = useNavigation();
-
   function navegaDetalhes() {
     navigation.navigate('Detalhes');
   }
-
   return (
     <View style={styles.container}>
       <Text>Tela HOME</Text>
       <Button title="Ir para detalhes" onPress={navegaDetalhes} />
+      <Button title="Abrir Drawer" onPress={() => navigation.openDrawer()} />
     </View>
   );
 }
