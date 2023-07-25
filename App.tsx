89ae5c445f114961c36eec65db355902f8a03f4d
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-25 19:22:41 -0300

    feat: start a new test project and create login system

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
-import Routes from './src/routes';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import Login from './src/components/Login';
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  if (!user) {
+    return <Login />;
+  }
   return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Text>DENTRO DA TELA DE TAREFAS</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 25,
+    paddingHorizontal: 10,
+    backgroundColor: '#F2F6FC',
+  },
+});
