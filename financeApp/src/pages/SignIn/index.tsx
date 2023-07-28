0565074b5a01cb4344d4847a4080de6e6dbfdf8e
miguel-barros-clear <miguel.barros.clear@gmail.com>
2023-07-28 06:43:08 -0300

    feat: allowing user to signin

--- financeApp/src/pages/SignIn/index.tsx
+++ financeApp/src/pages/SignIn/index.tsx
@@ -19,7 +19,11 @@ export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleLogin() { }
+  const { signIn } = useContext(AuthContext);
+
+  function handleLogin() {
+    signIn(email, password);
+  }
 
   return (
     <Background>
--- financeApp/src/routes/index.tsx
+++ financeApp/src/routes/index.tsx
@@ -1,13 +1,16 @@
-import React, { useContext } from 'react';
+mport { View, ActivityIndicator } from 'react-native';
 import { AuthContext } from '../contexts/auth';
 
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
 function Routes() {
-  const { signed } = useContext(AuthContext);
+  const { signed, loading } = useContext(AuthContext);
 
-  return signed ? <AppRoutes /> : <AuthRoutes />;
-}
-
-export default Routes;
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#131313" />
+      </View>
+    );
+  }
